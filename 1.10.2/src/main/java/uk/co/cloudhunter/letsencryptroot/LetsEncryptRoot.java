bf0cb0b2cbca020f57d06ddb8233e174e6802ccd
Aaron Mills <cloudhunter@btinternet.com>
2018-06-29 13:54:05 +0100

    Release candidate - now doesn't run if not needed.

--- 1.10.2/src/main/java/uk/co/cloudhunter/letsencryptroot/LetsEncryptRoot.java
+++ 1.10.2/src/main/java/uk/co/cloudhunter/letsencryptroot/LetsEncryptRoot.java
@@ -10,18 +10,41 @@ import java.io.InputStream;
 import java.net.URL;
 
 
-@Mod(modid = LetsEncryptRoot.MOD_ID, name = LetsEncryptRoot.NAME, version = LetsEncryptRoot.VERSION, clientSideOnly = true, acceptableRemoteVersions="*", acceptedMinecraftVersions = "*")
+@Mod(modid = LetsEncryptRoot.MOD_ID, name = LetsEncryptRoot.NAME, version = LetsEncryptRoot.VERSION, acceptableRemoteVersions="*", acceptedMinecraftVersions = "*")
 public class LetsEncryptRoot
 {
 
     public static final String MOD_ID = "letsencryptroot";
     public static final String NAME = "Let's Encrypt Root";
     public static final String VERSION = "@VERSION@";
-    public static final Logger logger = LogManager.getLogger("letsencryptroot");
+    public static Logger logger = LogManager.getLogger("letsencryptroot");
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
+        logger = event.getModLog();
+        String version = System.getProperty("java.version");
+        String majorVersion = version.substring(0, version.lastIndexOf("."));
+        int minorVersion = Integer.valueOf(version.substring(version.lastIndexOf("_") + 1));
+
+        switch (majorVersion)
+        {
+            case "1.7":
+                if (minorVersion >= 111)
+                {
+                    logger.info("Not running as Java version is at least Java 7u111.");
+                        return;
+                }
+                break;
+            case "1.8":
+                if (minorVersion >= 101)
+                {
+                    logger.info("Not running as Java version is at least Java 8u101.");
+                    return;
+                }
+                break;
+        }
+
         String body = "";
         try {
             logger.info("Adding Let's Encrypt certificate...");
--- 1.7.10/src/main/java/uk/co/cloudhunter/letsencryptroot/LetsEncryptRoot.java
+++ 1.7.10/src/main/java/uk/co/cloudhunter/letsencryptroot/LetsEncryptRoot.java
@@ -1,11 +1,12 @@
 package uk.co.cloudhunter.letsencryptroot;
 
-import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import net.minecraft.util.ResourceLocation;
-import org.apache.logging.log4j.LogManager;
+import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.Logger;
+
+import java.io.InputStream;
+import java.net.URL;
 
 @Mod(modid = LetsEncryptRoot.MOD_ID, name = LetsEncryptRoot.NAME, version = LetsEncryptRoot.VERSION, acceptableRemoteVersions="*")
 public class LetsEncryptRoot
@@ -14,11 +15,51 @@ public class LetsEncryptRoot
     public static final String MOD_ID = "letsencryptroot";
     public static final String NAME = "Let's Encrypt Root";
     public static final String VERSION = "@VERSION@";
-    public static final Logger logger = LogManager.getLogger("letsencryptroot");
+    public static Logger logger = null;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        LetsEncryptAdder.addLetsEncryptCertificate(LetsEncryptRoot.class.getResourceAsStream("assets/letsencryptroot/DSTRootCAX3.der"));
+        logger = event.getModLog();
+        String version = System.getProperty("java.version");
+        String majorVersion = version.substring(0, version.lastIndexOf("."));
+        int minorVersion = Integer.valueOf(version.substring(version.lastIndexOf("_") + 1));
+
+        switch (majorVersion)
+        {
+            case "1.7":
+                if (minorVersion >= 111)
+                {
+                    logger.info("Not running as Java version is at least Java 7u111.");
+                    return;
+                }
+                break;
+            case "1.8":
+                if (minorVersion >= 101)
+                {
+                    logger.info("Not running as Java version is at least Java 8u101.");
+                    return;
+                }
+                break;
+        }
+
+        String body = "";
+        try {
+            logger.info("Adding Let's Encrypt certificate...");
+            LetsEncryptAdder.addLetsEncryptCertificate();
+            logger.info("Done, attempting to connect to https://helloworld.letsencrypt.org...");
+            URL url = new URL("https://helloworld.letsencrypt.org");
+            InputStream inputStream = url.openStream();
+            body = IOUtils.toString(inputStream);
+        } catch (Exception e) {
+            logger.error("An error occurred whilst adding the Let's Encrypt root certificate. I'm afraid you wont be able to access resources with a Let's Encrypt certificate D:", e);
+        }
+
+        if (body.isEmpty())
+        {
+            logger.error("An unknown error occurred whilst adding the Let's Encrypt root certificate. I'm afraid you may not be able to access resources with a Let's Encrypt certificate D:");
+        } else {
+            logger.info("Done - you are now able to access resources with a Let's Encrypt certificate :D");
+        }
     }
 }
